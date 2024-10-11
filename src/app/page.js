36103666002e09f30b78a135d46deff1471b6563
Rafacosti <rafaelcosta005@gmail.com>
2024-10-11 17:34:01 -0300

    Adicionando todos os arquivos do projeto conversor-moedas

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Pagina from "@/components/Pagina";
+
+export default function HomePage() {
+  return (
+    <Pagina titulo="Conversor de moedas">
+      <p>Conversor de Real para outras moedas.</p>
+
+    </Pagina>
+  );
+}
